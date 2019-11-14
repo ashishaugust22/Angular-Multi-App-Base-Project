855e191ae809d09c2e648c5ce0d5430f5ea81a73
Ashish <ashish@isummation.com>
2019-11-14 15:02:23 +0530

    minor UI changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from "@angular/core";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
+import { StepsComponent } from './steps/steps.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, StepsComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
